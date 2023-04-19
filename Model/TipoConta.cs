85b0d414e99d7076bb9b1adf33071a6165e8bf3a
elopes89 <86383785+elopes89@users.noreply.github.com>
2023-04-19 02:55:07 -0300

    semana 3

---
+++ Model/TipoConta.cs
@@ -0,0 +1,7 @@
+namespace Semana1Floripa.Model;
+
+public enum TipoConta {
+
+    CORRENTE = 1,
+    POUPANCA = 2
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ExercicioSemana3Floripa.Model;
+using Semana1Floripa.Model;
+
+namespace projetoConsole;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        ContaBancaria contaOrigem = new ContaBancaria();
+        ContaBancaria contaDestino = new ContaBancaria();
+
+        contaOrigem.Saldo = 1000.0;
+        contaOrigem.Numero = 123;
+        contaDestino.Numero = 777;
+        // contaDestino.Depositar(445);
+
+        System.Console.WriteLine("Transferência: " , contaOrigem.Transferencia(contaDestino, 600.0));
+        System.Console.WriteLine(contaOrigem.Saldo);
+        System.Console.WriteLine(contaDestino.Saldo);
+        System.Console.WriteLine(contaDestino.Numero);
+    }
+
+}
+
